b02cf5bbd49de1c93491bd01601f960a7268eded
Allen Zhao <cz264@njit.edu>
2017-02-16 16:53:44 -0500

    finished layout

--- app/src/main/java/com/example/cz264/herolegend/Activity/MainActivity.java
+++ app/src/main/java/com/example/cz264/herolegend/Activity/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.cz264.herolegend.Activity;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.cz264.herolegend.Fragments.MainFragment;
 import com.example.cz264.herolegend.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
